62d8759442c86aac6630e554e3e2616f106decd9
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2019-06-09 23:34:32 +0300

    Badge for cart menu item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { CartModel } from './cart/components/models';
+import { CartService } from './cart/services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('appTitle')
   title: ElementRef<HTMLInputElement>;
-  cartTotal = 0;
+  cart: Promise<CartModel>;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cart = this.cartService.getCart();
+  }
 
   ngAfterViewInit() {
     this.title.nativeElement.innerHTML = 'Plants Shop';
