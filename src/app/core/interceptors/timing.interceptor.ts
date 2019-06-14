b96265981ad1d0dc0c052b00294064f3847a8350
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2019-06-14 12:51:44 +0300

    Indentation

--- src/app/core/interceptors/timing.interceptor.ts
+++ src/app/core/interceptors/timing.interceptor.ts
@@ -15,14 +15,13 @@ export class TimingInterceptor implements HttpInterceptor {
       // API to measure request latency https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
       const t0 = performance.now();
 
-      return next.handle(req)
-        .pipe(
-          map((res) => {
-            const t1 = performance.now();
-            console.log(`Call ${req.method} ${req.url} took ${t1 - t0} milliseconds.`);
-            return res;
-          }),
-        );
+      return next.handle(req).pipe(
+        map((res) => {
+          const t1 = performance.now();
+          console.log(`Call ${req.method} ${req.url} took ${t1 - t0} milliseconds.`);
+          return res;
+        }),
+      );
     }
 
     return next.handle(req);
